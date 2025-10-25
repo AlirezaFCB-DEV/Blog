f4c21075cc1ea348014dbed088b0ae393e0416fd
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2025-10-25 16:17:38 +0330

    feat : coding urls and accounts app views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def register_user(req):    
+    """
+    This view is used to register a new user to the system.
+    It takes an HTTP request object as an argument and returns
+    an HTTP response object.
+    The view is responsible for rendering the registration form
+    and handling the registration process.
+    """
+    pass
+
+def login_user(req) :
+    """
+    This view is used to log a user into the system.
+    It takes an HTTP request object as an argument and returns
+    an HTTP response object.
+    The view is responsible for rendering the login form
+    and handling the login process.
+    """
+    pass
+
+def logout_user(req) :
+    """
+    This view is used to log a user out of the system.
+    It takes an HTTP request object as an argument and returns
+    an HTTP response object.
+    The view is responsible for handling the logout process.
+    """
+    pass
+
+def profile_view(req) :
+    """
+    This view is used to show the user's profile page.
+    It takes an HTTP request object as an argument and returns
+    an HTTP response object.
+    The view is responsible for rendering the profile page
+    and handling any necessary logic.
+    """
+    pass
