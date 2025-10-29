56d5e1622e971bbb33bf0df0d2eaefa1a0d1f3a3
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2025-10-29 16:34:29 +0330

    feat : create delete view with authorization user

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,15 @@
-from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from .models import Post
 
 # Create your views here.
 
-def delete_post(req , post_id):
-    pass
+
+@login_required
+def delete_post(req, post_id):
+    post = Post.objects.get(pk=post_id)
+    if post.author != req.user:
+        return JsonResponse({"error ": "You are not authorized to delete this post"}, status=403)
+
+    post.delete()
+    return JsonResponse({"msg": "Post deleted successfully !!"}, status=201)
