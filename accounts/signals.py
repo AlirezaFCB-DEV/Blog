8aa56fc640fc42e82d22da3a064a99867083932f
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2025-10-30 10:53:37 +0330

    feat: create delete signal

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save , post_delete
 from django.contrib.auth import get_user_model
 from django.dispatch import receiver
 from .models import Profile
@@ -10,3 +10,11 @@ def create_profile_for_user (sender , instance , created , **kwargs):
     if created :
         Profile.objects.create(user=instance)
         print(f"Profile created for {instance.email}")
+    
+@receiver(post_delete , sender=User)
+def delete_profile_with_user (sender , instance , **kwargs) :
+    try :
+        instance.profile.delete()
+        print(f"Profile deleted for {instance.email}")
+    except :
+        pass
