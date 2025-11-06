7ecd5ec92950fef408b6b00aaadf0bef2214f6d3
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2025-11-06 20:24:54 +0330

    feat : create a new app for a new exercise and create a new post model

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published = models.BooleanField(default=False)
+    created_at = mdoels.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
