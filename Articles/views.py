acd40b6eb9b43bc0fb3d914072baf835b8dbed36
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2025-11-12 16:09:02 +0330

    feat : Create Generic view exc

--- Articles/views.py
+++ Articles/views.py
@@ -1,6 +1,7 @@
 from django.views import View
 from django.http import JsonResponse
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView , CreateView
 
 from .models import Article
 # Create your views here.
@@ -11,3 +12,10 @@ class Article_List_View(ListView) :
     def render_to_response(self, context, **response_kwargs):
          data = list(context["object_list"].values())
          return JsonResponse(data , safe=False)
+     
+class Article_Create_View(CreateView) :
+    model = Article
+    fields = ["title" , "content"]
+    success_url = reverse_lazy("article_list")
+    
+    
